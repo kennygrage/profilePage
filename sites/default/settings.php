e39e8558b9ec24c09215f2326c36a5ed652364d3
Kenny Grage <kennygrage@gmail.com>
2015-10-12 15:25:30 -0700

    customize page, add zen theme

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -212,7 +212,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'profile',
+      'username' => 'admin',
+      'password' => 'password',
+      'host' => '127.0.0.1',
+      'port' => '8889',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -244,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'pbKIORoLVnOlrfqMAzkHm0dxs4_UToSTKB3IHHRvM4o';
 
 /**
  * Base URL (optional).
